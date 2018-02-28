4c80d5e269afffe3e6d43e739a03619cdcb85487
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-28 12:32:58 -0800

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Chris Dean on 2018-02-28.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Chris Dean on 2018-02-28.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
